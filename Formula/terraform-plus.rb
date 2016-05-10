57419d0036d6368b164761f0308551a0cf6455a4
Ross Hattori <rhattori@saymedia.com>
2016-05-10 10:58:56 -0700

    release: Update to terraform-plus v0.6.16+0.1

--- Formula/terraform-plus.rb
+++ Formula/terraform-plus.rb
@@ -1,9 +1,9 @@
 class TerraformPlus < Formula
-  version "0.6.15+0.1"
+  version "0.6.16+0.1"
   desc "A custom Terraform distro with extra plugins we use at Say Media"
   homepage "https://github.com/saymedia/terraform-plus"
   url "https://github.com/saymedia/terraform-plus/releases/download/v#{version}/terraform-plus-v#{version.to_s.sub! "+", "."}-darwin.tar.xz"
-  sha256 "a2358ad1b5161f0eed570d095dc26f598a1dc9db9a1aa399cdd26de91f1bde04"
+  sha256 "9810ae22302a759f8dae7871ba647e9d05cd2adc037bd8c8beac465fa3839f83"
 
   depends_on "xz"
   conflicts_with "terraform", :because => "terraform-plus overrides the terraform binary"
